a02d0ea54f06b0a46cd32e315951ccc6dcd653d9
Ashish-1104 <111485873+Ashish-1104@users.noreply.github.com>
2024-01-21 23:33:50 +0530

    Update db.js

--- utlis/db.js
+++ utlis/db.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 const URI = process.env.MONGODB_URI;
-console.log(URI)
-console.log(process.env.A)
-// const URI = "mongodb+srv://ashish:MYmongodb@cluster0.b2r09wv.mongodb.net/register_data?retryWrites=true&w=majority";
 const connectDB = async () => {
     try {
         await mongoose.connect(URI);
